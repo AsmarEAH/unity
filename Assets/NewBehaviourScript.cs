e78789f0d2bebf40037ab2e10b41b74674091404
AsmarEAH <34270794+AsmarEAH@users.noreply.github.com>
2018-02-14 18:20:23 +0100

    test
    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public string app;
+	private string app2;
+	public Text text;
+	public sprite sp;
+	public bool b;
+
+	// Use this for initialization
+	void Start () {
+		Text.text = app;
+		app2 = 'hello';
+		text.text = app2; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
